233699ea968f02c7ef8bd079925a2f9c815cc4b8
jing <541247371@qq.com>
2021-07-29 15:26:55 +0800

    实现了注册功能

--- ph/urls.py
+++ ph/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 # import product_hunt.products.views
 # from product_hunt import products
 import products.views
+
+# from product_hunt import account
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', products.views.product_list),
+    path('', products.views.product_list,name='主页'),
+    path('acount/', include('account.urls')),
 ]
